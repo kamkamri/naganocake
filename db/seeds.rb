c9eb82c561946871882379c8395aeb13bf50e9ab
EC2 Default User <ec2-user@ip-172-31-19-174.ap-northeast-1.compute.internal>
2022-08-05 11:40:47 +0000

    [Add]アソシエーション

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# あらかじめデータベースに登録したいデータを記載　adminデータ
+Admin.create!(
+  email: 'admin@co.jp' ,
+  encrypted_password: 'admin'
+  )
