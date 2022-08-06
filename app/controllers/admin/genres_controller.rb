6ab637ec08fbfff33ffa782673513c902cba0f96
EC2 Default User <ec2-user@ip-172-31-19-174.ap-northeast-1.compute.internal>
2022-08-06 13:07:10 +0000

    [Add]genre機能実装

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,7 +1,43 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      flash[:notice] = "You have created genre successfully."
+      # genre#indexにリダイレクト
+      redirect_to admin_genres_path
+    else
+      # gnre#index にrender
+      @genres = Genre.all
+      render :index
+    end
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
+
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      flash[:notice] = "You have updated genre successfully."
+      # genrek#indexにリダイレクト
+      redirect_to admin_genres_path
+    else
+      # gnre#index にrender
+      render :edit
+    end
+  end
+
+  # ストロングパラメータ
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
 end
