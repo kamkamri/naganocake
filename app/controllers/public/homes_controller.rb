e5e2e2a71c04daae2632993bd0c66a9855a2fc6a
EC2 Default User <ec2-user@ip-172-31-19-174.ap-northeast-1.compute.internal>
2022-08-15 11:37:43 +0000

    [Add]TOP,ABOUTページ実装

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,8 @@
 class Public::HomesController < ApplicationController
   def top
+    @genres = Genre.all
+    # ステータスが販売中の商品の新着4件を表示
+    @items = Item.where(is_active: "true").order('id DESC').limit(4)
   end
 
   def about
