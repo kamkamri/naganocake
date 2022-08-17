be6e2f01bc54f5bc026c24a9fe6f22284c25d6d1
EC2 Default User <ec2-user@ip-172-31-19-174.ap-northeast-1.compute.internal>
2022-08-17 02:04:44 +0000

    [Add]注文履歴がめん、admin会員一覧

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -73,9 +73,12 @@ class Public::OrdersController < ApplicationController
 
 
   def index
+    @orders = current_customer.orders
   end
 
   def show
+    @order = current_customer.orders.find(params[:id])
+    @order_items = @order.order_items
   end
 
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,9 +3,11 @@ class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
 
   enum payment_method: {credit_card:0, transfer:1 }
+  enum order_status: {waiting:0, comfirmed:1, doing:2, shipping_preparation:3, shipped:4 }
 
    # 住所結合
   def address_display
     '〒 ' + postal_code + '　' + address
+
   end
 end
